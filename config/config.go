53a094aeb0a683f30b2685326cefc28170436a12
Vladimir <myweb11@mail.ru>
2026-02-19 22:57:25 +0700

    fix load env

--- config/config.go
+++ config/config.go
@@ -37,69 +37,30 @@ type DbConfig struct {
 func Load() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("not found .env file")
+		log.Println(".env file not found, using environment variables")
 	}
 
 	// db config
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := getEnvVar("DB_HOST")
+	dbPort := getEnvVar("DB_PORT")
+	dbName := getEnvVar("DB_NAME")
+	dbUser := getEnvVar("DB_USER")
+	dbPassword := getEnvVar("DB_PASSWORD")
 	dsn := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		dbUser, dbPassword, dbHost, dbPort, dbName,
 	)
 
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		log.Fatal("HTTP_PORT is not set")
-	}
-
-	publicUrl := os.Getenv("PUBLIC_URL")
-	if publicUrl == "" {
-		log.Fatal("PUBLIC_URL is not set")
-	}
-
-	env := os.Getenv("APP_ENV")
-	if env == "" {
-		log.Fatal("APP_ENV is not set")
-	}
-
-	s3Bucket := os.Getenv("S3_BUCKET")
-	if s3Bucket == "" {
-		log.Fatal("S3_BUCKET is not set")
-	}
-
-	s3Key := os.Getenv("S3_KEY")
-	if s3Key == "" {
-		log.Fatal("S3_KEY is not set")
-	}
-
-	s3Secret := os.Getenv("S3_SECRET")
-	if s3Secret == "" {
-		log.Fatal("S3_SECRET is not set")
-	}
-
-	s3PublicUrl := os.Getenv("S3_PUBLIC_URL")
-	if s3PublicUrl == "" {
-		log.Fatal("S3_PUBLIC_URL is not set")
-	}
-
-	storageType := os.Getenv("STORAGE_TYPE")
-	if storageType == "" {
-		log.Fatal("STORAGE_TYPE is not set")
-	}
-
-	botToken := os.Getenv("BOT_TOKEN")
-	if botToken == "" {
-		log.Fatal("BOT_TOKEN is not set")
-	}
-
-	jwtSecret := os.Getenv("JWT_SECRET")
-	if jwtSecret == "" {
-		log.Fatal("JWT_SECRET is not set")
-	}
+	httpPort := getEnvVar("HTTP_PORT")
+	publicUrl := getEnvVar("PUBLIC_URL")
+	env := getEnvVar("APP_ENV")
+	s3Bucket := getEnvVar("S3_BUCKET")
+	s3Key := getEnvVar("S3_KEY")
+	s3Secret := getEnvVar("S3_SECRET")
+	s3PublicUrl := getEnvVar("S3_PUBLIC_URL")
+	storageType := getEnvVar("STORAGE_TYPE")
+	botToken := getEnvVar("BOT_TOKEN")
+	jwtSecret := getEnvVar("JWT_SECRET")
 
 	return &Config{
 		Env: env,
@@ -120,4 +81,12 @@ func Load() *Config {
 		},
 		JwtSecret: jwtSecret,
 	}
+}
+
+func getEnvVar(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("%s is not set", key)
+	}
+	return value
 }
